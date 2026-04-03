41fdcc4fdb56efd2b6abe612aef5ec7b74ca5c72
Vandana Yadav <yadavvk2010@gmail.com>
2026-04-03 06:21:20 +0530

    Add single.php template for displaying individual blog posts

--- wp-content/themes/aruix/single.php
+++ wp-content/themes/aruix/single.php
@@ -1,31 +1,26 @@
 <?php
     // Include the header template file (header.php) which typically contains the HTML head, navigation, etc.
-    get_header(); 
+    get_header();
 ?>
-
-<h1>Blog Posts</h1>
-
-<!-- Display a list of blog posts -->
+<!-- Display the single blog post -->
 <?php
     // Check if there are any posts to display in the current query
     if (have_posts()) :
 ?>
+
     <?php
         // Start a loop to iterate through each post in the query
         while (have_posts()) : the_post();
     ?>
-        <h2>
-            <?php // Displays the title of the current post as an h2 heading ?>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </h2>
-        <p><?php the_content(); // Displays the full content of the current post ?></p>
+    
+        <h1><?php the_title(); // Displays the title of the current post as an h1 heading ?></h1>
+        <p><em>Posted on <?php the_date(); // Displays the publication date of the current post ?></em></p>
+        <?php the_content(); // Displays the full content of the current post ?>
 
     <?php endwhile; ?>
-<?php else : ?>
-    <p>No posts found</p>
 <?php endif; ?>
 
-<?php 
+<?php
     // Include the footer template file (footer.php) which typically contains closing HTML tags, scripts, etc.
     get_footer();
 ?>
